9973f10fc0fa7d9fae29b1b54caf265d2dda1520
g.ibarraa <g.ibarraas@gmail.com>
2025-09-17 18:00:34 -0600

    estructura mínima de HomeScreen con secciones vacías

--- app/src/main/java/com/example/jayalafoodapp/feature/home/HomeScreen.kt
+++ app/src/main/java/com/example/jayalafoodapp/feature/home/HomeScreen.kt
@@ -8,35 +8,36 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.jayalafoodapp.feature.home.components.HeaderSection
 
 @Composable
 fun HomeScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .padding(horizontal = 16.dp, vertical = 12.dp)
-    ) {
-        HeaderSection(userName = "Daniel")
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(
-            text = "Nuestras categorías",
-            style = MaterialTheme.typography.headlineMedium
-        )
-        Spacer(modifier = Modifier.height(24.dp))
-        Text(
-            text = "Busca los mejores restaurantes",
-            style = MaterialTheme.typography.headlineMedium
-        )
-        Spacer(modifier = Modifier.height(24.dp))
-        Text(
-            text = "Nuestras mejores comidas",
-            style = MaterialTheme.typography.headlineMedium
-        )
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(horizontal = 16.dp, vertical = 12.dp)
+        ) {
+            Text(
+                text = "Nuestras categorías",
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(
+                text = "Busca los mejores restaurantes",
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(
+                text = "Nuestras mejores comidas",
+                style = MaterialTheme.typography.headlineMedium
+            )
+        }
     }
 }
